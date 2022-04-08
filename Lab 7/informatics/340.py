63447b2b507d73c02dd67c38fa48a44dddbb52d3
Ayazhan <kuanysh.ayazhan070503@gmail.com>
2022-04-08 06:23:49 +0600

    -

--- Lab 7/informatics/340.py
+++ Lab 7/informatics/340.py
@@ -1,4 +1,5 @@
-a = int(input())
-while i <= n :
-        if (n % i==0) :
-            print (i, end = " ")
+n = int(input())
+
+for i in range(1, n+1):
+    if n % i == 0:
+        print(i, end=' ')
